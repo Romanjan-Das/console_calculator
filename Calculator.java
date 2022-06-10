3c25c3b76d8b0c68cbb2e00bcc0d430f1a10fb8a
romanjan-das <romanjandas@outlook.com>
2022-06-10 23:04:14 +0530

    basic console calculator working

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,27 @@
-public class Calculator {
-    public static void main(String[] args){
+import java.util.Scanner;;
 
+public class Calculator {
+    private static String input = "";
+    private static int i = 0;
+    private static boolean wrong_input=false;
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        input = s.nextLine();
+        input=input+"=";
+        while (i < input.length()) {
+            StringFormation.verify_input(input.charAt(i));
+            if (!StringFormation.allow) {
+                wrong_input=true;
+                break;
+            }
+            i++;
+        }
+        if(wrong_input){
+            System.out.println("wrong input");
+        }
+        else{
+            System.out.println("answer: " + StringFormation.input_string);
+        }
     }
 }
